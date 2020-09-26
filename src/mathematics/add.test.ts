b319cbe50ec004fc1cfc376cb416663b59f734ae
Simon Skinner <simon@vult.uk>
2020-09-26 13:30:32 +0000

    1.1.7

--- src/mathematics/add.test.ts
+++ src/mathematics/add.test.ts
@@ -1,13 +1,13 @@
 import { add } from './add';
 
-test("Mathematics / Add", () => {
-    const incrementByOne = add(1);
-    expect(incrementByOne(19)).toBe(20);
-    expect(incrementByOne(0)).toBe(1);
-    expect(incrementByOne(-33)).toBe(-32);
+test('Mathematics / Add', () => {
+  const incrementByOne = add(1);
+  expect(incrementByOne(19)).toBe(20);
+  expect(incrementByOne(0)).toBe(1);
+  expect(incrementByOne(-33)).toBe(-32);
 
-    const addFive = add(5);
-    expect(addFive(10)).toBe(15);
-    expect(addFive(0)).toBe(5);
-    expect(addFive(-21)).toBe(-16);
-})
+  const addFive = add(5);
+  expect(addFive(10)).toBe(15);
+  expect(addFive(0)).toBe(5);
+  expect(addFive(-21)).toBe(-16);
+});
