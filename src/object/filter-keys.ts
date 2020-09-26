8df2aeeb69833456604a2e1b3dda5ff0baf1434c
Simon Skinner <simon@vult.uk>
2020-09-26 14:43:39 +0000

    Ability to filter an object by keys

---
+++ src/object/filter-keys.ts
@@ -0,0 +1,3 @@
+import { sumArray } from "../array";
+
+export const filterKeys = (arr: string[]) => (obj: any) => Object.keys(obj).filter(k => arr.includes(k)).reduce((p, c) => ({...p, [c]: obj[c]}), {})
