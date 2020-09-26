eeeea007bef6c27cbc1a1e0f9a9ef83fd0911c1f
Simon Skinner <simon@vult.uk>
2020-09-26 14:43:50 +0000

    1.1.9

--- src/object/filter-keys.test.ts
+++ src/object/filter-keys.test.ts
@@ -1,34 +1,34 @@
 import { filterKeys } from './filter-keys';
 
-test("Object -> Filter Keys", () => {
-    const singleObject = {
-        itemOne: "Item One",
-        itemTwo: "Item Two",
-        itemThree: 3,
-        itemFour: true,
-        itemFive: {
-            subItem1: "Sub 1",
-            subItem2: "Sub 2",
-            subItem3: 3,
-            subItem4: false,
-        }
-    };
+test('Object -> Filter Keys', () => {
+  const singleObject = {
+    itemOne: 'Item One',
+    itemTwo: 'Item Two',
+    itemThree: 3,
+    itemFour: true,
+    itemFive: {
+      subItem1: 'Sub 1',
+      subItem2: 'Sub 2',
+      subItem3: 3,
+      subItem4: false,
+    },
+  };
 
-    const firstFilter = filterKeys(["itemOne", "itemTwo", "itemFour"]);
-    expect(firstFilter(singleObject)).toEqual({
-        itemOne: "Item One",
-        itemTwo: "Item Two",
-        itemFour: true,
-    });
+  const firstFilter = filterKeys(['itemOne', 'itemTwo', 'itemFour']);
+  expect(firstFilter(singleObject)).toEqual({
+    itemOne: 'Item One',
+    itemTwo: 'Item Two',
+    itemFour: true,
+  });
 
-    const subFilter = filterKeys(['itemFour', 'itemFive']);
-    expect(subFilter(singleObject)).toEqual({
-        itemFour: true,
-        itemFive: {
-            subItem1: "Sub 1",
-            subItem2: "Sub 2",
-            subItem3: 3,
-            subItem4: false,
-        }
-    });
+  const subFilter = filterKeys(['itemFour', 'itemFive']);
+  expect(subFilter(singleObject)).toEqual({
+    itemFour: true,
+    itemFive: {
+      subItem1: 'Sub 1',
+      subItem2: 'Sub 2',
+      subItem3: 3,
+      subItem4: false,
+    },
+  });
 });
--- src/object/filter-keys.ts
+++ src/object/filter-keys.ts
@@ -1,3 +1,6 @@
-import { sumArray } from "../array";
+import { sumArray } from '../array';
 
-export const filterKeys = (arr: string[]) => (obj: any) => Object.keys(obj).filter(k => arr.includes(k)).reduce((p, c) => ({...p, [c]: obj[c]}), {})
+export const filterKeys = (arr: string[]) => (obj: any) =>
+  Object.keys(obj)
+    .filter((k) => arr.includes(k))
+    .reduce((p, c) => ({ ...p, [c]: obj[c] }), {});
