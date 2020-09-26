5b15348aeef5bcc8ed32146c5d90460a6b12a6de
Simon Skinner <simon@vult.uk>
2020-09-26 13:19:58 +0000

    1.1.6

--- src/string/join.test.ts
+++ src/string/join.test.ts
@@ -1,9 +1,9 @@
 import { join } from './join';
 
-test("String / Join", () => {
-    const joinWithFullstop = join('.');
-    expect(joinWithFullstop(['www','hello','com'])).toBe('www.hello.com');
-    
-    const createFilePath = join('/');
-    expect(createFilePath(['path','to','file.pdf'])).toBe('path/to/file.pdf');
+test('String / Join', () => {
+  const joinWithFullstop = join('.');
+  expect(joinWithFullstop(['www', 'hello', 'com'])).toBe('www.hello.com');
+
+  const createFilePath = join('/');
+  expect(createFilePath(['path', 'to', 'file.pdf'])).toBe('path/to/file.pdf');
 });
